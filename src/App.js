c223b32cac3ebab105213ed5c199145b54b2f9b6
Udylia <udylia@yandex.ru>
2023-09-25 12:28:37 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import image from './todo.jpg';
+import imageTwo from './todoOne.jpg';
+import {Todo} from './Todo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <img src={image} alt='Todo' width='250px' />
+      </div>
+      <div className='container'>
+        <h1>Todo list</h1>
+      </div>
+      <Todo />
+      <div className='container'>
+        <img src={imageTwo} alt='Todo' width='250px' />
+      </div>
     </div>
   );
 }
